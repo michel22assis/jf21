1b6b3e2056b3034ae191f06307d014ad152a4de3
PC <PC@DESKTOP-75BB8EC>
2024-10-31 21:43:14 -0300

    Scritp de conexão com o banco

--- fj21/src/br/com/caelum/jdbc/ConnectionFactory.java
+++ fj21/src/br/com/caelum/jdbc/ConnectionFactory.java
@@ -1,5 +1,15 @@
 package br.com.caelum.jdbc;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
 
 public class ConnectionFactory {
-
+	
+	public Connection getConnetion() {
+		try {
+			return DriverManager.getConnection("jdbc:mysql://localhost/fj21", "root", "");
+		}catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
